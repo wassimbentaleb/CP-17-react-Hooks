480056403e917babd4995b13da06eb4370d8f463
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-04-08 12:56:38 +0100

    react-Hooks

---
+++ src/Component/Movielist/MovieList.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import Card from "../Card/Card";
+const MovieList = ({search, movielist, searchRating}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        margin: "7%",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+      }}
+    >
+      {movielist
+        .filter(
+          (el) =>
+            el.name.toUpperCase().includes(search.toUpperCase()) &&
+            el.rating >= searchRating
+        )
+        .map((el) => (
+          <Card
+            key={el.id}
+            image={el.image}
+            name={el.name}
+            type={el.type}
+            description={el.description}
+            rating={el.rating}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default MovieList;
